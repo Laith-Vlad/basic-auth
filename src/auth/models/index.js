d958dd945702f7bef2b398c51a38fd92feea5130
Laith-Vlad <laithxfg13@gmail.com>
2023-07-02 14:29:42 +0000

    devb

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -1,8 +1,21 @@
 'use strict';
 
-const {Sequelize, DataTypes} = require('sequelize');
-const POSTGRS_URL = process.env.DB_URL;
+require('dotenv').config(); // Add dotenv configuration
 
-const sequelize = new Sequelize(POSTGRS_URL, {});
+const { Sequelize, DataTypes } = require('sequelize');
+const userSchema = require('./users.model.js');
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DB_URL;
+
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 
 module.exports = { sequelize, DataTypes };
